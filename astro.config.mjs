94a2e7cd52fde400b594dc64315276fe3174b7d2
Noah Hess <noahhess@Noahs-MacBook-Air.local>
2026-09-09 12:26:44 -0700

    Updated domain name and page references.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nhess10.github.io',
-  base: '/Portfolio',
+  site: 'https://noahhess.com',
+  base: '/',
 });
